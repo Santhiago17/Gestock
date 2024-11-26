74a8ec7a0f298e6235987e80ef76a8e96cad85bd
Santhiago Santos <santhiagosantos7@gmail.com>
2024-11-25 23:20:49 -0300

    feat: Cria rotas de produto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "teste";
+});
+
+// Produtos
+Route::prefix('produtos')->group(function () {
+    Route::post('/',[ProductController::class, 'store']);
+    Route::get('/',[ProductController::class, 'index']);
+    Route::post('/',[ProductController::class, 'edit']);
+    Route::post('/',[ProductController::class, 'delete']);
 });
